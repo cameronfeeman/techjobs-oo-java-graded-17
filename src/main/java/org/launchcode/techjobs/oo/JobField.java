5c0f7c7b4d91b014493e8ab734ce817ab724879b
cameronfeeman <139170459+cameronfeeman@users.noreply.github.com>
2023-11-03 12:31:22 -0400

    Finished and passed tests through Task 6

---
+++ src/main/java/org/launchcode/techjobs/oo/JobField.java
@@ -0,0 +1,57 @@
+package org.launchcode.techjobs.oo;
+
+import java.util.Objects;
+
+public abstract class JobField {
+
+    private int id;
+    private static int nextId = 1;
+    private String value;
+
+    public JobField() {
+        id = nextId;
+        nextId++;
+    }
+
+    public JobField(String value) {
+        this();
+        this.value = value;
+    }
+
+    // TODO: Add a custom toString() method that returns the data stored in 'value'.
+//Custom toString, equals, and hashCode methods:
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        JobField that = (JobField) o;
+        return id == that.id;
+    }
+
+
+
+    // Getters and Setters:
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, value);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
